e98c93d83e83906662f4f6e0961efdfbf95e8742
Temparo <164712201+Temparo@users.noreply.github.com>
2025-07-22 15:58:12 +0800

    Try to fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./src/**/*.{html,vue,js,ts,jsx,tsx}",
     ],
     theme: {
         fontFamily: {'sans': ['Inter', 'PingFang SC', 'Avenir', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],},
